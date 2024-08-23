c8f0c8a88cc1e5b65aa0d23ca02a1624c72369ce
Eric Domenzain Morales <eric.domenzain@utng.edu.mx>
2024-08-23 13:10:31 -0600

    Cambios generales

---
+++ sistema-ventas-api/src/routes/auth.routes.ts
@@ -0,0 +1,52 @@
+import { Router } from "express";
+import { authController } from "../controllers/auth.controller";
+import { authRules } from "../rules/auth.rules";
+import { validate } from "../middlewares/validator.check";
+
+class AuthRoutes {
+
+    public router: Router;
+
+    constructor() {
+        this.router = Router();
+        this.config();
+    }
+
+    /**
+     * @swagger
+     * definitions:
+     *  User:
+     *      type: object
+     *      properties:
+     *          username:
+     *              type: string
+     *          password:
+     *              type: string
+     */
+    config() {
+        /**
+         * @swagger
+         * /api/auth:
+         *  post:
+         *      tags: ["Credenciales"]
+         *      summary: Log In
+         *      description: Inicio de sesion del usuario
+         *      produces:
+         *          - application/json
+         *      parameters:
+         *          - in: body
+         *            name: Credentials
+         *            description: Usuario y contraseña del usuario
+         *            schema:
+         *              $ref: '#/definitions/User'
+         *            required: true
+         *      responses:
+         *          200:
+         *              description: Exito
+         */
+        this.router.post("/", authRules(), [validate], authController.iniciarSesion);
+    }
+}
+
+const authRoutes = new AuthRoutes();
+export default authRoutes.router;
